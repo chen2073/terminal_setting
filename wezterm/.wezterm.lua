33c31bab2327c5e0764442f563d71a38f73fe5e4
James Chen <jchen@cpacketnetworks.com>
2026-03-17 12:14:30 -0700

    update wez

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,15 +1,20 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "catppuccin-macchiato"
 
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
-config.font_size = 13
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 11
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
-config.default_domain = 'WSL:Ubuntu'
+config.default_domain = "WSL:Ubuntu"
+
+config.automatically_reload_config = true
+
+config.window_close_confirmation = "NeverPrompt"
 
 return config
